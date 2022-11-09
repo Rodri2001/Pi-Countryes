67b65a1251dd3d891b80569c92b32e64a62535a6
Rodrigo <rodrigodamianmartinez2001@gmail.com>
2022-11-09 17:04:22 -0300

    back end

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,7 @@
 const { Router } = require('express');
+const { getAllCountries, getCountries } = require('../Controllers/getAllCountries');
+const { getCountriesDetail } = require('../Controllers/getCountriesDetail');
+const { getAllCountriesInDb } = require('../Controllers/savedCountries');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -7,6 +10,9 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+router.get('/countries',getCountries)
+router.get('/countries/:id', getCountriesDetail)
 
 
 module.exports = router;
